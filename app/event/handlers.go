4519679beadbd3820e5121a21db765a30456e366
ymwz-max <YM@ankr.com>
2023-09-22 12:15:47 +0800

    fix: update WithdrawalFinalizerRequestFinalizeWithdrawal

--- app/event/handlers.go
+++ app/event/handlers.go
@@ -590,8 +590,10 @@ var bridgeEventZKSyncFinalizeWithdrawErc20Handle eventHandlerFunction = func(eve
 		logrus.Warningf("112bridgeEventZKSyncFinalizeWithdrawErc20Handle parsed.Methods[\"finalizeWithdrawals\"] error. transactionHash:%s, err:%s", event.transactionHash, err)
 		return err
 	}
-	var outs []bridge.WithdrawalFinalizerRequestFinalizeWithdrawal
+	var outs = []bridge.WithdrawalFinalizerRequestFinalizeWithdrawal{}
 	withdrawParsed.Methods["finalizeWithdrawals"].Inputs.Copy(&outs, withdraws)
+	logrus.Warningf("111bridgeEventZKSyncFinalizeWithdrawErc20Handle len:%d", len(outs))
+
 	mysqlClient, ok := transaction.getRawClient().(*gorm.DB)
 	if !ok {
 		logrus.Fatalln("Weird! convert raw transaction client error")
