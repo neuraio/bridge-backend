8b5beb60914318508a4e0dadbb0eee299e7c0402
ymwz-max <YM@ankr.com>
2023-09-22 12:52:38 +0800

    fix: update WithdrawalFinalizerRequestFinalizeWithdrawal

--- app/event/handlers.go
+++ app/event/handlers.go
@@ -585,7 +585,6 @@ var bridgeEventZKSyncFinalizeWithdrawErc20Handle eventHandlerFunction = func(eve
 	}
 	logrus.Warningf("111bridgeEventZKSyncFinalizeWithdrawErc20Handle parsed.Methods[\"finalizeWithdrawal\"] error. transactionHash:%s, err:%s", event.transactionHash, err)
 	withdrawParsed, _ := abi.JSON(strings.NewReader(bridge.WithdrawalFinalizerABI))
-	logrus.Warningf("111bridgeEventZKSyncFinalizeWithdrawErc20Handle data:%s", hexutil.Encode(tx.Data()))
 
 	withdraws, err := withdrawParsed.Methods["finalizeWithdrawals"].Inputs.Unpack(tx.Data()[4:])
 	if err != nil {
